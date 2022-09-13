c5d19e3968f8c94212c3148178224a83d437a8a8
Thibault Berthelin <61558171+DevoWeb60@users.noreply.github.com>
2022-09-13 16:00:09 +0200

    fix for linux

--- toConvert.sh
+++ toConvert.sh
@@ -8,20 +8,20 @@ exportFolderExist=false
 fileExtensionToConvert=test
 
 # FUNCTIONS 
-function checkIfFolderExist() {
+checkIfFolderExist() {
     if [ -d $folderToExport ]
     then
         exportFolderExist=true
     fi
 }
 
-function convertToWebp() {
+convertToWebp() {
     local fileName=${1%.*}
     local fileExtension=${1##*.}
     cwebp -q 60 $fileName.$fileExtension -o $folderContainingFilesToConvert/$folderToExport/$fileName.webp
 }
 
-function getExtension() {
+getExtension() {
     local fileExtension=${1##*.}
     fileExtensionToConvert=$fileExtension
 }
@@ -29,7 +29,7 @@ function getExtension() {
 # MAIN
 checkIfFolderExist
 
-if [ $exportFolderExist == false ]
+if [ $exportFolderExist = false ]
 then
     #Creating the export folder
     mkdir $folderToExport
@@ -39,9 +39,9 @@ fi
 # Loop on all elements in path of files to convert ($1)
 cd $folderContainingFilesToConvert
 for file in * ; do
-    if [ -f $file ] && [ "$file" != $scriptName ]; then
+    if [ -f $file ] && [ "$file" != "$scriptName" ]; then
         getExtension $file
-        if [ $fileExtensionToConvert == "jpg" ] || [ $fileExtensionToConvert == "png" ] || [ $fileExtensionToConvert == "jpeg" ]; then
+        if [ "$fileExtensionToConvert" = "jpg" ] || [ "$fileExtensionToConvert" = "png" ] || [ "$fileExtensionToConvert" = "jpeg" ]; then
             convertToWebp $file
         else 
             echo "Le fichier $file n'est pas une image"
